e970e13b44b40ea99c95308eba11ffe80a34484a
Octal <landrytchouambia@gmail.com>
2025-03-29 11:50:49 +0100

    allo

--- src/pages/blogs/detail-produit.tsx
+++ src/pages/blogs/detail-produit.tsx
@@ -1,7 +1,6 @@
 import { Layout } from "@/ui/components/layout/layout";
 import { Seo } from "@/ui/components/seo/Seo";
 import { AudreySlackView } from "@/ui/modules/landing-page/components/audrey-slack/audrey-slack-view";
-import { CurrentCourseView } from "@/ui/modules/landing-page/components/current-course/current-course-view";
 
 export default function Blogs() {
   return (
